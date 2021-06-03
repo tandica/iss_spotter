4b2eb9d84e76fff1fed292707081e7576dedbfb8
tandica <williams.tandica@gmail.com>
2021-06-03 01:05:35 +0000

    added index2 and promised files

---
+++ iss_promised.js
@@ -0,0 +1,29 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function(callback) {
+    return request(`https://api.ipify.org/?format=json`);
+};
+
+const fetchCoordsByIP = function(body) {
+    const ip = JSON.parse(body).ip;
+    return request(`https://freegeoip.app/json/${ip}`);
+  };
+  
+const fetchISSFlyOverTimes = function(body) {
+    const dur = JSON.parse(body).duration;
+    const rise = JSON.parse(body).risetime;
+    console.log(JSON.parse(body).response.duration)
+    return request(`http://api.open-notify.org/iss-pass.json?lat=${dur}&lon=${rise}`);
+  };
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+      .then(fetchCoordsByIP)
+      .then(fetchISSFlyOverTimes)
+      .then((data) => {
+      const { response } = JSON.parse(data);
+      console.log(response);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
